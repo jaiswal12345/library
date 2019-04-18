ad7dc32367ae4dec60ec959423abd4a517dec9df
Piyush Jaiswal <piyush.jaiswal@ad.kaleyra.local>
2019-04-18 15:02:20 +0530

    adding filters

--- controllers/book.ctrl.js
+++ controllers/book.ctrl.js
@@ -1,94 +1,105 @@
 var Book = require('../models/book.model');
-function BookCtrl(){    
-    this.get = function(req,res){
-        var count;
+function BookCtrl() {
+    this.get = function (req, res) {
+        var filter = {};
+        if (req.query.name){
+            filter.name = new RegExp('^' + req.query.name + '$', "i");
+        }
+        if (req.query.author){
+            filter.author = new RegExp('^' + req.query.author + '$', "i");
+        }
+        if (req.query.count){
+            filter.count = req.query.count;
+        }
+            var count;
         var pageSize = +req.params.pageSize || 10;
         var pageIndex = +req.params.pageIndex || 0;
         //deferred execution:
-        var query = Book.find().skip(pageIndex*pageSize).limit(pageSize);
+        var query = Book.find(filter).skip(pageIndex * pageSize).limit(pageSize);
+        console.log(query);
         var count;
-        Book.count().exec()
-        .then(function(result){
-            count = result;
-            return query.exec()
-        .then(function(book){  
-                var meta = {
-                    totalRecords : count,
-                    totalPages : Math.ceil(count/pageSize)
-                };
-                var response = {
-                    metaData : meta,
-                    book : book
-                };
-                res.status(200);
-                res.json(response);
-            }).catch(function(error){
-                res.status(500);
-                 res.json({
-                    message : "Internal Server Error",
-                    status : "500"
-                });
-            });         
-        })
+        Book.count(filter).exec()
+            .then(function (result) {
+                count = result;
+                return query.exec()
+                    .then(function (book) {
+                        var meta = {
+                            totalRecords: count,
+                            totalPages: Math.ceil(count / pageSize)
+                        };
+                        var response = {
+                            metaData: meta,
+                            book: book
+                        };
+                        res.status(200);
+                        res.json(response);
+                    }).catch(function (error) {
+                        res.status(500);
+                        res.json({
+                            message: "Internal Server Error",
+                            status: "500"
+                        });
+                    });
+            })
     };
 
     //insert
-    this.save = function(req,res){
+    this.save = function (req, res) {
         var book = new Book(req.body);
-        book.save(function(err,result){
-            if(err){
+        book.save(function (err, result) {
+            if (err) {
                 res.status(500);
                 res.json({
-                    message : "Internal Server Error",
-                    status : "500"
+                    message: "Internal Server Error",
+                    status: "500"
                 });
             }
-            else{
+            else {
                 res.status(201);
                 res.json({
-                    message : "saved successfully",
-                    status : "201"
+                    message: "saved successfully",
+                    status: "201"
                 });
             }
         });
-   };
+    };
 
-   this.put = function(req,res){
-    var id = req.params.id;
-    Book.findByIdAndUpdate(id,req.body,function(err,result){
-        if (err){
-         res.status(500);
-         res.json({
-                    message : "Internal Server Error",
-                    status : "500"
+    this.put = function (req, res) {
+        var id = req.params.id;
+        Book.findByIdAndUpdate(id, req.body, function (err, result) {
+            if (err) {
+                res.status(500);
+                res.json({
+                    message: "Internal Server Error",
+                    status: "500"
                 });
-        } else{
-         res.status(200);//deleted
-         res.json({
-                    message : "updated successfully",
-                    status : "201"
+            } else {
+                res.status(200);//deleted
+                res.json({
+                    message: "updated successfully",
+                    status: "201"
                 });
-        }
-    });   
- };
-   //delete function
-   this.delete = function(req,res){
-       var id = req.params.id;
-       Book.findByIdAndRemove(id,function(err,result){
-           if (err){
-            res.status(500);
-            res.json({
-                    message : "Internal Server Error",
-                    status : "500"
+            }
+        });
+    };
+    //delete function
+    this.delete = function (req, res) {
+        var id = req.params.id;
+        Book.findByIdAndRemove(id, function (err, result) {
+            if (err) {
+                res.status(500);
+                res.json({
+                    message: "Internal Server Error",
+                    status: "500"
                 });
-           } else{
-            res.status(204);//deleted
-             res.json({
-                    message : "deleted successfully",
-                    status : "201"
+            } else {
+                res.status(204);//deleted
+                res.json({
+                    message: "deleted successfully",
+                    status: "201"
                 });
-           }
-       });   
+            }
+        });
     };
 }
 module.exports = new BookCtrl();
--- routers/book.router.js
+++ routers/book.router.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 var bookCtrl = require('../controllers/book.ctrl');
 router.get("/",bookCtrl.get);
-// router.get("/:pageIndex/:pageSize",bookCtrl.get);
+router.get("/:pageIndex/:pageSize",bookCtrl.get);
 // router.get("/:id",bookCtrl.getByID);
 router.post('/',bookCtrl.save);
 router.delete("/:id",bookCtrl.delete);
